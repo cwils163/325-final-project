69957fbe45c3325f8fe30715da4d2c6b68a594c6
cwils163 <cwils163@vols.utk.edu>
2024-10-22 11:26:59 -0400

    add code used to make graphs for data analysis

--- model.py
+++ model.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import nltk
+#nltk.download('punkt_tab') # RUN WITH THIS ONCE
+from nltk.tokenize import word_tokenize
 
-data = pd.read_csv('Data.csv')
-print(data.head())
+# imports csv data to pandas datafram
+data = pd.read_csv('Data.csv', index_col=0)
+
+# removes NAN values from dataframe
+data.dropna(inplace=True)
+#print(data.head())
+
